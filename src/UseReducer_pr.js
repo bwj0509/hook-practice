3bb83dc8d4db00542715ebf7561a5e0893f2cd5a
Woojin <bwj59@naver.com>
2022-04-30 11:50:29 +0900

    pr

---
+++ src/UseReducer_pr.js
@@ -0,0 +1,62 @@
+import React, { useReducer, useRef, useState } from 'react';
+
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'ADDUSER':
+            return [...state, { username: action.user, id: action.id }]
+        case 'REMOVEUSER':
+            return [...state.filter(user => user.id != action.id)]
+        default:
+            return state
+    }
+}
+
+const initialState = [
+
+]
+
+
+function UseReducer_pr() {
+
+    const [user, setUser] = useState('')
+    const nextId = useRef(1);
+
+    const changeUser = (e) => {
+        setUser(e.target.value)
+    }
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    const addUser = () => {
+        dispatch({
+            type: 'ADDUSER', user, id: nextId.current
+        })
+        nextId.current += 1;
+    }
+
+    const onRemove = (e) => {
+        dispatch({
+            type: 'REMOVEUSER', id: e.target.value
+        })
+    }
+
+    return (
+        <div>
+            <h1>출석부</h1>
+            <span>총학생수 : {state.length}</span>
+            <div>
+                <input placeholder='이름을 입력하세요' value={user} onChange={changeUser} />
+                <button onClick={addUser}>추가</button>
+                <ul>
+                    {state.map((user) => (
+                        <li key={user.id}>{user.username}<button value={user.id} onClick={onRemove}>삭제</button></li>
+                    ))}
+                    {console.log(state)}
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default UseReducer_pr;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import UseMemo from './UseMemo';
 import UseReducer from './UseReducer';
+import UseReducer_pr from './UseReducer_pr';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UseReducer />
+    <UseReducer_pr />
   </React.StrictMode>
 );
 
