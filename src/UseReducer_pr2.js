a3972ba0bbf851f75ad77a2079b6ce3ef080a21d
Woojin <bwj59@naver.com>
2022-05-08 21:19:06 +0900

    pr

--- src/UseReducer_pr2.js
+++ src/UseReducer_pr2.js
@@ -7,15 +7,16 @@ const initialState = [
 const reducer = (state, action) => {
     switch (action.type) {
         case 'ADDUSER':
-            return [...state, { id: action.id, username: action.name, age: action.age }]
+            return [...state, { id: action.id, username: action.name, age: action.age, active: false }]
         case 'REMOVEUSER':
             return [...state.filter((user) => (String(user.id) !== action.id))]
+        case 'ACTIVE':
+            return state.map((user) => (user.id == action.id ? { ...user, active: !user.active } : user))
         default:
             return state
     }
-
 }
-
+// [...state] === state
 
 
 function UseReducer_pr2() {
@@ -45,16 +46,32 @@ function UseReducer_pr2() {
             type: 'REMOVEUSER', id: e.target.value
         })
     }
+
+    const onActive = (e) => {
+        dispatch({
+            type: 'ACTIVE', id: e.target.value
+        })
+    }
     return (
         <div>
             <input placeholder='이름을 입력하세요' value={name} onChange={changeUser} />
             <input placeholder='나이를 입력하세요' value={age} onChange={changeAge} />
             <button onClick={onAdd}>추가</button>
+            <hr></hr>
+            <span>{`유저 : ${state.length}명`}</span><br></br>
+            <span>{`활성화된 유저 : ${state.filter(user => user.active == true).length}명`}</span>
             {console.log(state)}
             <ul>
                 {state.map((user) => (
-                    <>
-                        <li>{user.age}{user.username}<button value={user.id} onClick={onRemove}>삭제</button></li>
+                    <>{user.active
+                        ? <li style={{ color: 'red' }} key={user.id} value={user.id} onClick={onActive}>{user.username}{user.age}
+                            <button value={user.id} onClick={onRemove}>삭제</button>
+                        </li>
+                        : <li key={user.id} value={user.id} onClick={onActive}>{user.username}{user.age}
+                            <button value={user.id} onClick={onRemove}>삭제</button>
+                        </li>
+                    }
+
                     </>
                 ))}
             </ul>
